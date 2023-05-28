27f21392ea20d1539c85d29cb3c824d56845fe3d
Wu Jane <jane72418@gmail.com>
2023-05-28 23:52:55 +0800

    add 排序篩選、搜尋功能，並改善類別篩選功能

--- assets/js/all.js
+++ assets/js/all.js
@@ -3,31 +3,59 @@ const url = "https://hexschool.github.io/js-filter-data/data.json";
 const showList = document.querySelector(".showList");
 const select = document.querySelector("#js-select");
 const filterBtn = document.querySelector(".button-group");
+const searchBtn = document.querySelector(".search");
+const crop = document.querySelector("#crop");
 
-let data = [];
 let tempData = [];
-axios.get(url).then((res) => {
-  console.log(res.data);
-  data = res.data;
 
-  filterBtn.addEventListener("click", (e) => {
-    console.log(filterBtn.children);
-    Array.from(filterBtn.children).forEach((childBtn) => {
-      childBtn.classList.remove("btn-type-active");
+axios
+  .get(url)
+  .then((res) => {
+    let apiData = res.data;
+    tempData = apiData;
+    //渲染出所有 item
+    renderData(apiData);
+    return apiData;
+  })
+  .then((apiData) => {
+    console.log(apiData);
+    filterBtn.addEventListener("click", (e) => {
+      // console.log(filterBtn.children);
+      //類別按鈕樣式切換
+      Array.from(filterBtn.children).forEach((childBtn) => {
+        childBtn.classList.remove("btn-type-active");
+      });
+      e.target.classList.add("btn-type-active");
+      //篩選出符合類別的資料
+      if (e.target.dataset.type === "all") {
+        renderData(apiData);
+      } else {
+        categoryFilter(apiData, e.target.dataset.type);
+        if (tempData.length > 0) {
+          renderData(tempData);
+        } else {
+          showList.innerHTML = `<td colspan="7" class="text-center p-3">請輸入並搜尋想比價的作物名稱^＿^</td>`;
+        }
+      }
     });
-    e.target.classList.add("btn-type-active");
-    categoryFilter(e.target.dataset.type);
-    if (tempData.length > 0) {
+    select.addEventListener("change", (e) => {
+      sortUpPrice(tempData, e.target.value);
       renderData(tempData);
-    } else {
-      showList.innerHTML = `<td colspan="7" class="text-center p-3">請輸入並搜尋想比價的作物名稱^＿^</td>`;
-    }
+    });
+    console.log(crop.value);
+    searchBtn.addEventListener("click", (e) => {
+      if (crop.value != "" && tempData.length > 0) {
+        inputFilter(apiData, crop.value);
+        renderData(tempData);
+      } else {
+        showList.innerHTML === "查詢不到當日的交易資訊QQ";
+      }
+    });
   });
-});
 
+//頁面渲染
 function renderData(data) {
-  let str = "";
-  console.log(data);
+  str = "";
   data.forEach((item) => {
     str += `<tr>
     <td width="15%">${item["作物名稱"]}</td>
@@ -87,14 +115,27 @@ function renderData(data) {
   showList.innerHTML = str;
 }
 
-function categoryFilter(target) {
+function categoryFilter(data, target) {
   tempData = data.filter((item) => {
     return target === item["種類代碼"];
   });
 }
 
-function sortUpPrice() {
-  data.sort((a, b) => {
-    return a - b;
+function sortUpPrice(data, price) {
+  tempData = data.sort((a, b) => {
+    return b[price] - a[price];
+  });
+}
+
+function inputFilter(data, input) {
+  //只會篩選出一模一樣的內容
+  // tempData = data.filter((item) => {
+  //   return item["作物名稱"] === input;
+  // });
+  //部分文字符合即可
+  tempData = data.filter((item) => {
+    if (item["作物名稱"]) {
+      return item["作物名稱"].indexOf(input) > -1;
+    }
   });
 }
